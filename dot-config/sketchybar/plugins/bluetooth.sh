89b3c8a017b40adb02bae95a6739e9aecc846270
kushvinth <kushvinth.m@gmail.com>
2026-07-21 17:35:21 +0530

    fix(sketchybar): wifi and bluetooth

--- dot-config/sketchybar/plugins/bluetooth.sh
+++ dot-config/sketchybar/plugins/bluetooth.sh
@@ -1,51 +1,55 @@
 #!/bin/bash
 
+export PATH="/run/current-system/sw/bin:$HOME/.nix-profile/bin:/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+
 source "$HOME/.config/sketchybar/icons.sh"
 source "$HOME/.config/sketchybar/colors.sh"
 
 update() {
-  # Check if blueutil is installed
-  if ! command -v blueutil &> /dev/null; then
-    sketchybar --set $NAME icon="$BLUETOOTH_OFF" label="No blueutil"
-    return
-  fi
+  local DATA
+  DATA=$(system_profiler SPBluetoothDataType -json 2>/dev/null | \
+    /usr/bin/jq -r '
+      .SPBluetoothDataType[0] |
+      if .controller_properties.controller_state == "attrib_on" then
+        if has("device_connected") then
+          "connected " + ([.device_connected[] | keys[0]] | length | tostring)
+        else
+          "on"
+        end
+      else
+        "off"
+      end
+    ' 2>/dev/null)
 
-  STATE=$(blueutil -p)
-  
-  if [ "$STATE" = "0" ]; then
-    # Bluetooth is OFF
-    sketchybar --set $NAME icon="$BLUETOOTH_OFF" icon.color="$GREY" label.drawing=off
-  else
-    # Bluetooth is ON
-    # Check for connected devices
-    # blueutil --connected returns a JSON list or list of devices. 
-    # We can count lines or check output.
-    CONNECTED_DEVICES=$(blueutil --connected | wc -l | tr -d ' ')
-    
-    if [ "$CONNECTED_DEVICES" -gt "0" ]; then
-      # Connected
-      sketchybar --set $NAME icon="$BLUETOOTH_CONNECTED" icon.color="$BLUE" label="$CONNECTED_DEVICES" label.drawing=on
-    else
-      # On but not connected
-      sketchybar --set $NAME icon="$BLUETOOTH_ON" icon.color="$WHITE" label.drawing=off
-    fi
-  fi
+  case "$DATA" in
+    connected*)
+      local COUNT="${DATA#connected }"
+      sketchybar --set "$NAME" icon="$BLUETOOTH_CONNECTED" icon.color="$BLUE" \
+        label="$COUNT" label.drawing=on
+      ;;
+    on)
+      sketchybar --set "$NAME" icon="$BLUETOOTH_ON" icon.color="$WHITE" label.drawing=off
+      ;;
+    off|*)
+      sketchybar --set "$NAME" icon="$BLUETOOTH_OFF" icon.color="$GREY" label.drawing=off
+      ;;
+  esac
 }
 
 click() {
-  # Toggle bluetooth on click? Or open prefs?
-  # Let's toggle for now as it's a common use case, or we could just open settings.
-  # "blueutil -p toggle" toggles power.
-  
-  CURRENT_STATE=$(blueutil -p)
-  if [ "$CURRENT_STATE" = "0" ]; then
-    blueutil -p 1
+  local BLUEUTIL="/run/current-system/sw/bin/blueutil"
+  if [ -x "$BLUEUTIL" ]; then
+    local CURRENT_STATE
+    CURRENT_STATE=$("$BLUEUTIL" -p 2>/dev/null)
+    if [ "$CURRENT_STATE" = "0" ]; then
+      "$BLUEUTIL" -p 1
+    else
+      "$BLUEUTIL" -p 0
+    fi
+    update
   else
-    blueutil -p 0
+    open "x-apple.systempreferences:com.apple.BluetoothSettings"
   fi
-  
-  # Trigger immediate update
-  update
 }
 
 case "$SENDER" in
--- dot-config/sketchybar/plugins/wifi.sh
+++ dot-config/sketchybar/plugins/wifi.sh
@@ -1,72 +1,50 @@
 #!/bin/bash
 
-#update() {
-#  source "$CONFIG_DIR/icons.sh"
-#  INFO="$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | awk -F ' SSID: '  '/ SSID: / {print $2}')"
-#  LABEL="$INFO ($(ipconfig getifaddr en0))"
-#  ICON="$([ -n "$INFO" ] && echo "$WIFI_CONNECTED" || echo "$WIFI_DISCONNECTED")"
-#
-#  sketchybar --set $NAME icon="$ICON" label="$LABEL"
-#}
-#
-#click() {
-#  CURRENT_WIDTH="$(sketchybar --query $NAME | jq -r .label.width)"
-#
-#  WIDTH=0
-#  if [ "$CURRENT_WIDTH" -eq "0" ]; then
-#    WIDTH=dynamic
-#  fi
-#
-#  sketchybar --animate sin 20 --set $NAME label.width="$WIDTH"
-#}
-#
-#case "$SENDER" in
-#  "wifi_change") update
-#  ;;
-#  "mouse.clicked") click
-#  ;;
-#esac
+export PATH="/run/current-system/sw/bin:$HOME/.nix-profile/bin:/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+
+CONFIG_DIR="${CONFIG_DIR:-$HOME/.config/sketchybar}"
+source "$CONFIG_DIR/icons.sh"
 
 update() {
-    CONFIG_DIR="${CONFIG_DIR:-$HOME/.config/sketchybar}"
-    source "$CONFIG_DIR/icons.sh"
+  local DATA
+  DATA=$(system_profiler SPAirPortDataType -json 2>/dev/null | \
+    jq -r '
+      .SPAirPortDataType[0].spairport_airport_interfaces[] |
+      select(._name == "en0") |
+      if .spairport_status_information == "spairport_status_connected" then
+        .spairport_current_network_information._name
+      else
+        ""
+      end
+    ' 2>/dev/null)
 
-    # Get SSID reliably (trim spaces)
-    #    INFO="$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I |
-    #        grep ' SSID:' | sed 's/.*SSID: //')"
-    INFO=$(networksetup -listallhardwareports |
-        awk '/Wi-Fi|AirPort/{getline; print $2}')
-    # Detect active interface IP (en0 or en1 fallback)
-    # IP_ADDR=$(ipconfig getifaddr en0)
-    IP_ADDR=$(ipconfig getifaddr $(route get default | awk '/interface:/{print $2}'))
-    [ -z "$IP_ADDR" ] && IP_ADDR=$(ipconfig getifaddr en1)
+  local IP_ADDR
+  IP_ADDR=$(ipconfig getifaddr en0 2>/dev/null)
 
-    # Set icon based on WiFi connected or not
-    if [ -n "$INFO" ]; then
-        ICON="$WIFI_CONNECTED"
-        LABEL="$INFO ($IP_ADDR)"
-    else
-        ICON="$WIFI_DISCONNECTED"
-        LABEL="Not Connected"
-    fi
+  if [ -n "$DATA" ] && [ "$DATA" != "null" ]; then
+    ICON="$WIFI_CONNECTED"
+    LABEL="$DATA${IP_ADDR:+ ($IP_ADDR)}"
+  else
+    ICON="$WIFI_DISCONNECTED"
+    LABEL="Not Connected"
+  fi
 
-    sketchybar --set "$NAME" icon="$ICON" label="$LABEL"
+  sketchybar --set "$NAME" icon="$ICON" label="$LABEL"
 }
 
 click() {
-    CURRENT_WIDTH="$(sketchybar --query "$NAME" | jq -r .label.width)"
+  CURRENT_WIDTH="$(sketchybar --query "$NAME" | jq -r .label.width)"
 
-    if [ "$CURRENT_WIDTH" -eq 0 ]; then
-        WIDTH="dynamic"
-    else
-        WIDTH=0
-    fi
+  if [ "$CURRENT_WIDTH" -eq 0 ]; then
+    WIDTH="dynamic"
+  else
+    WIDTH=0
+  fi
 
-    sketchybar --animate sin 20 --set "$NAME" label.width="$WIDTH"
+  sketchybar --animate sin 20 --set "$NAME" label.width="$WIDTH"
 }
 
 case "$SENDER" in
 "wifi_change") update ;;
 "mouse.clicked") click ;;
 esac
-
