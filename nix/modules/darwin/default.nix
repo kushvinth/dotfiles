52de7a2e62c19002dec3cba371ffe0383574bd8d
kushvinth <kushvinth.m@gmail.com>
2026-07-21 12:23:08 +0530

    feat(nix): daemon brew services

--- nix/modules/darwin/default.nix
+++ nix/modules/darwin/default.nix
@@ -4,6 +4,7 @@
 {
   imports = [
     ./system.nix
+    ./services.nix
     ./etc.nix
     ./homebrew.nix
     ./activation.nix
---
+++ nix/modules/darwin/services.nix
@@ -0,0 +1,49 @@
+{ config, lib, pkgs, ... }:
+let
+  brewPrefix = "/opt/homebrew/bin";
+in
+{
+  launchd.user.agents = {
+    "org.nixos.skhd" = {
+      command = "${brewPrefix}/skhd";
+      serviceConfig = {
+        KeepAlive = true;
+        RunAtLoad = true;
+        ProcessType = "Interactive";
+        EnvironmentVariables = {
+          PATH = "${brewPrefix}:/usr/local/bin:/usr/bin:/bin";
+        };
+        StandardOutPath = "/tmp/skhd.log";
+        StandardErrorPath = "/tmp/skhd.err";
+      };
+    };
+
+    "org.nixos.yabai" = {
+      command = "${brewPrefix}/yabai";
+      serviceConfig = {
+        KeepAlive = true;
+        RunAtLoad = true;
+        ProcessType = "Interactive";
+        EnvironmentVariables = {
+          PATH = "${brewPrefix}:/usr/local/bin:/usr/bin:/bin";
+        };
+        StandardOutPath = "/tmp/yabai.log";
+        StandardErrorPath = "/tmp/yabai.err";
+      };
+    };
+
+    "org.nixos.sketchybar" = {
+      command = "${brewPrefix}/sketchybar";
+      serviceConfig = {
+        KeepAlive = true;
+        RunAtLoad = true;
+        ProcessType = "Interactive";
+        EnvironmentVariables = {
+          PATH = "${brewPrefix}:/usr/local/bin:/usr/bin:/bin";
+        };
+        StandardOutPath = "/tmp/sketchybar.log";
+        StandardErrorPath = "/tmp/sketchybar.err";
+      };
+    };
+  };
+}
