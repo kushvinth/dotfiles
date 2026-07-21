51ae5278e2d6c55f80b2572cea0f89bfd40d85db
kushvinth <kushvinth.m@gmail.com>
2026-07-21 12:24:44 +0530

    remove(nvim): the homepage

--- dot-config/nvim/lua/plugins/z-dashboard.lua
+++ dot-config/nvim/lua/plugins/z-dashboard.lua
@@ -1,74 +1,76 @@
 return {
-  -- disable alpha/snacks dashboard
-  { "folke/snacks.nvim", opts = { dashboard = { enabled = false } } },
-
-  -- enable mini.starter with custom header
-  {
-    "nvim-mini/mini.starter",
-    version = false,
-    event = "VimEnter",
-    opts = function()
-      local logo = table.concat({
-        [[    ██╗     █████╗ ██████╗ ██████╗      ██╗██████╗ ███████╗ ]],
-        [[    ██║    ██╔══██╗██╔══██╗██╔══██╗     ██║██╔══██╗██╔════╝ ]],
-        [[    ██║    ███████║██████╔╝██████╔╝     ██║██║  ██║█████╗   ]],
-        [[    ██║    ██╔══██║██╔══██╗██╔══██╗     ██║██║  ██║██╔══╝   ]],
-        [[    ███████╗██║  ██║██║  ██║██████╔╝     ██║██████╔╝███████╗ ]],
-        [[    ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝      ╚═╝╚═════╝ ╚══════╝ ]],
-      }, "\n")
-
-      local pad = string.rep(" ", 22)
-      local new_section = function(name, action, section)
-        return { name = name, action = action, section = pad .. section }
-      end
-
-      local starter = require("mini.starter")
-      return {
-        evaluate_single = true,
-        header = logo,
-        items = {
-          new_section("Find file",       LazyVim.pick(),                        "Telescope"),
-          new_section("New file",        "ene | startinsert",                   "Built-in"),
-          new_section("Recent files",    LazyVim.pick("oldfiles"),              "Telescope"),
-          new_section("Find text",       LazyVim.pick("live_grep"),             "Telescope"),
-          new_section("Config",          LazyVim.pick.config_files(),           "Config"),
-          new_section("Restore session", [[lua require("persistence").load()]], "Session"),
-          new_section("Lazy Extras",     "LazyExtras",                          "Config"),
-          new_section("Lazy",            "Lazy",                                "Config"),
-          new_section("Quit",            "qa",                                  "Built-in"),
+    sections = {
+        { section = "header" },
+        {
+            pane = 2,
+            section = "terminal",
+            cmd = "colorscript -e square",
+            height = 5,
+            padding = 1,
         },
-        content_hooks = {
-          starter.gen_hook.adding_bullet(pad .. "░ ", false),
-          starter.gen_hook.aligning("center", "center"),
+        { section = "keys",  gap = 1, padding = 1 },
+        {
+            pane = 2,
+            icon = " ",
+            desc = "Browse Repo",
+            padding = 1,
+            key = "b",
+            action = function()
+                Snacks.gitbrowse()
+            end,
         },
-      }
-    end,
-    config = function(_, config)
-      if vim.o.filetype == "lazy" then
-        vim.cmd.close()
-        vim.api.nvim_create_autocmd("User", {
-          pattern = "MiniStarterOpened",
-          callback = function()
-            require("lazy").show()
-          end,
-        })
-      end
-
-      local starter = require("mini.starter")
-      starter.setup(config)
-
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "LazyVimStarted",
-        callback = function(ev)
-          local stats = require("lazy").stats()
-          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-          local pad_footer = string.rep(" ", 8)
-          starter.config.footer = pad_footer .. "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
-          if vim.bo[ev.buf].filetype == "ministarter" then
-            pcall(starter.refresh)
-          end
+        function()
+            local in_git = Snacks.git.get_root() ~= nil
+            local cmds = {
+                {
+                    title = "Notifications",
+                    cmd = "gh notify -s -a -n5",
+                    action = function()
+                        vim.ui.open("https://github.com/notifications")
+                    end,
+                    key = "n",
+                    icon = " ",
+                    height = 5,
+                    enabled = true,
+                },
+                {
+                    title = "Open Issues",
+                    cmd = "gh issue list -L 3",
+                    key = "i",
+                    action = function()
+                        vim.fn.jobstart("gh issue list --web", { detach = true })
+                    end,
+                    icon = " ",
+                    height = 7,
+                },
+                {
+                    icon = " ",
+                    title = "Open PRs",
+                    cmd = "gh pr list -L 3",
+                    key = "P",
+                    action = function()
+                        vim.fn.jobstart("gh pr list --web", { detach = true })
+                    end,
+                    height = 7,
+                },
+                {
+                    icon = " ",
+                    title = "Git Status",
+                    cmd = "git --no-pager diff --stat -B -M -C",
+                    height = 10,
+                },
+            }
+            return vim.tbl_map(function(cmd)
+                return vim.tbl_extend("force", {
+                    pane = 2,
+                    section = "terminal",
+                    enabled = in_git,
+                    padding = 1,
+                    ttl = 5 * 60,
+                    indent = 3,
+                }, cmd)
+            end, cmds)
         end,
-      })
-    end,
-  },
+        { section = "startup" },
+    },
 }
