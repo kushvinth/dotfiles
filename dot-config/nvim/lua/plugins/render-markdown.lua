b5bdfb52fcc53abe6838958e8b37259dc214b919
kushvinth <kushvinth.m@gmail.com>
2026-08-10 12:54:43 +0530

    add(nvim): render markdown

---
+++ dot-config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-mini/mini.nvim",
+    },
+    ft = { "markdown", "Avante" },
+    cmd = { "RenderMarkdown" },
+    opts = {},
+  },
+}
