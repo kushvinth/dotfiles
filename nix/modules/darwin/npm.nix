0870e24542c32cea2bf5abab947d6ac9e9699908
kushvinth <kushvinth.m@gmail.com>
2026-08-13 11:58:41 +0530

    fix(nix): remove npm activation script - npm not available during activation

--- nix/modules/darwin/npm.nix
+++ nix/modules/darwin/npm.nix
@@ -6,11 +6,4 @@
     prettier
     nodejs
   ];
-
-  system.activationScripts.postActivation.text = ''
-    if ! command -v gnhf &> /dev/null; then
-      echo "Installing gnhf..."
-      npm install -g gnhf
-    fi
-  '';
 }
